0e11736992fa22852407a0205f4bbed40e77b94d
Cmoney <cascherer42@gmail.com>
2018-08-02 17:53:16 -0500

    base and pseudo-code start

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+var triviaGame = {
+    hardMode: {
+        baseGame: {
+            
+        }
+    }
+}
+//!~~ Hard Mode Pseudo Code
+//Need a timer to countdown from 10 seconds. This resets every time you answer a question.
+    //if the time runs out, you lose the round
+//Need a question bank that has questions with four "Possible" answers, and a true question
+// 
