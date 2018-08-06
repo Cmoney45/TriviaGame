ad77b9ee9b6a9cf3182756e3cbcb1f6b6d75d147
Cmoney <cascherer42@gmail.com>
2018-08-05 23:17:35 -0500

    Need click function for the answers and a final reset

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,63 +1,99 @@
 var triviaGame = {
     anyGameOptions: {
-        gameRunning: false,
+        timerRunning: false,
+        questionOnBoard: false,
     },
     hardMode: {
         baseGame: {
             timerLength: 10,
-            // timerCount: triviaGame.hardMode.baseGame.timerLength,
+            timerReset: 10,
             timerInterval: "",
             qAnswered: 0,
             qCorrect: 0,
-            // Reset game to base board, use once game is finished.
+            newQuest: function(){
+                setTimeout(this.runGame, 2000)
+            },
+//** Reset game to base board, use once game is finished.
             resetGame: function(){
                 this.questionsAnswered = 0;
-                this.questionsCorrectly = 0;
             },
-            // Iniate the game
+//** Initiate the game
             runGame: function(){
+                const tHMbg = triviaGame.hardMode.baseGame;
+                tHMbg.timerLength = tHMbg.timerReset
+
                 clearInterval(this.timerInterval);
-                $("#timeStamp").html(`<p>Time Remaining: ${triviaGame.hardMode.baseGame.timerLength} Seconds</p>`)
+                $("#timeStamp").html(`<p>Time Remaining: ${tHMbg.timerLength} Seconds</p>`)
                 
-                triviaGame.hardMode.baseGame.createQuestionHTML();
-                triviaGame.hardMode.baseGame.timerInterval = setInterval(triviaGame.hardMode.baseGame.decrement, 1000);
+                tHMbg.chooseQuestion();
+                tHMbg.createQuestionHTML();
+                tHMbg.timerInterval = setInterval(tHMbg.decrement, 1000);
             },
-            // Timer function to decrease
+            checkCondition: function(selection) {
+                this.newQuest();
+
+            },
+            onwards: function(){
+                const tHMbg = triviaGame.hardMode.baseGame;
+
+                tHMbg.timerLength = tHMbg.timerReset;
+                tHMbg.chooseQuestion();
+                tHMbg
+            },
+//** Timer function to decrease
             decrement: function() {
-                triviaGame.hardMode.baseGame.timerLength--;
-                $("#timeStamp").html(`<p>Time Remaining: ${triviaGame.hardMode.baseGame.timerLength} Seconds</p>`)
+                const tHMbg = triviaGame.hardMode.baseGame;
+                tHMbg.timerLength--;
+                $("#timeStamp").html(`<p>Time Remaining: ${tHMbg.timerLength} Seconds</p>`)
 
-                if(triviaGame.hardMode.baseGame.timerLength === 0) {
-                    triviaGame.hardMode.baseGame.timerStop();
+                if(tHMbg.timerLength === 0) {
+                    tHMbg.timerStop();
 
-                    triviaGame.hardMode.baseGame.timesUp();
+                    tHMbg.timesUp();
+                    triviaGame.anyGameOptions.timerRunning = false;
                 }
             },
-            //Timer Stop function
+            //** Timer Stop function
             timerStop: function() {
-                //TBD
+                clearInterval(triviaGame.hardMode.baseGame.timerInterval)
             },
-            // Function for when the Timer runs out
+            //** Function for when the Timer runs out
             timesUp: function() {
+                const tHMqAcQ = triviaGame.hardMode.questionBank.active.currentQuestion;
                 $("#question").html(`<p>Times up!</p>`);
                 $("#responses").empty();
+                $("#responses").html(`<p>The correct response was ${tHMqAcQ.guesses[tHMqAcQ.answer]}</p>`);
+                triviaGame.hardMode.baseGame.checkCondition();
+                triviaGame.hardMode.baseGame.timerLength = triviaGame.hardMode.baseGame.timerReset;
             },
-            // Function to choose which question should be up
+            //** Function to choose which question should be up
             chooseQuestion: function() {
-                //TBD
+                const tHMqA = triviaGame.hardMode.questionBank.active;
+                const number = Math.floor(Math.random() * tHMqA.activeBank.length)
+                if( tHMqA.activeBank.length === 0) {
+                    triviaGame.hardMode.baseGame.completion();
+                    return;
+                }
+                if( tHMqA.questionsAnswered.lenth >= 0){
+                    tHMqA.questionsAnswered.push(tHMqA.currentQuestion.splice(0,1));
+                }
+                tHMqA.currentQuestion = tHMqA.activeBank[number]
+                tHMqA.activeBank.splice(number,1);
+                tHMqA.activeBank.activeQuestionCount = tHMqA.activeBank.length
             },
-            // Handles HTML creation of the current question.
+            //** Handles HTML creation of the current question.
             createQuestionHTML: function() {
+                const tHMqAcQ = triviaGame.hardMode.questionBank.active.currentQuestion;
                 $("#question").empty();
-                $("#question").html(`<p>${triviaGame.hardMode.questionBank.active.currentQuestion.question}</p>`)
+                $("#question").html(`<p>${tHMqAcQ.question}</p>`)
 
                 let count = 1;
                 $("#responses").empty();
-                triviaGame.hardMode.questionBank.active.currentQuestion.guesses.forEach(ele => {
-                    // Create variable of true or false, if true prepend or if false append
+                tHMqAcQ.guesses.forEach(ele => {
+            // TODO: Create variable of true or false, if true prepend or if false append
                     let choiceButton = $(`<button>`);
                     $(choiceButton).addClass("btn btn-primary btn-lg btn-block mycss-AnswerButton");
-                    $(choiceButton).attr("id",`answer${count}`);
+                    $(choiceButton).attr("id",`${count}`);
                     $(choiceButton).attr("type","button");
                     $(choiceButton).text(ele)
                     $("#responses").append(choiceButton);
@@ -68,40 +104,37 @@ var triviaGame = {
         },
         questionBank: {
             active: {
-                activeBank: {},
-                currentQuestion: {
-
-                },
-                questionsAnswered: {},
+                activeBank: [],
+                currentQuestion: [],
+                questionsAnswered: [],
             },
-            bankOne: {
-                question1: {
-                    question: `Question 1`,
-                    guesses: ["Fish","Blue","Yellow","Yellow-2"],
-                    answer: "3"
-                },
-                question2: {
-                    question: `Question 2`,
-                    guesses: ["Q","R","S","Batman Symbol"],
-                    answer: "3"
-                },
-                question3: {
-                    question: `Question 3`,
-                    guesses: ["Crocodile","Bear","Wuzzy","Fuzzy"],
-                    answer: "3"
-                },
-                question4: {
-                    question: `Question 4`,
-                    guesses: ["1","2","3","4"],
-                    answer: "3"
-                },
-                question5: {
-                    question: `Question 5`,
-                    guesses: ["1","2","3","4"],
-                    answer: "3"
-                },
-            }
-
+            bankOne: [
+                    question1 = {
+                        question: `Question 1`,
+                        guesses: ["Fish","Blue","Yellow","Yellow-2"],
+                        answer: "0"
+                    },
+                    question2 = {
+                        question: `Question 2`,
+                        guesses: ["Q","R","S","Batman Symbol"],
+                        answer: "3"
+                    },
+                    question3 = {
+                        question: `Question 3`,
+                        guesses: ["Crocodile","Bear","Wuzzy","Fuzzy"],
+                        answer: "0"
+                    },
+                    question4 = {
+                        question: `Question 4`,
+                        guesses: ["1","2","3","4"],
+                        answer: "3"
+                    },
+                    question5 = {
+                        question: `Question 5`,
+                        guesses: ["1","2","3","4"],
+                        answer: "3"
+                    }
+            ],
         }
     }
 }
@@ -110,8 +143,8 @@ var triviaGame = {
 $("#startGame").on("click",triviaGame.hardMode.baseGame.runGame)
 
 // testing createQuestionHTML(), need active to be... active
-triviaGame.hardMode.questionBank.active.currentQuestion = triviaGame.hardMode.questionBank.bankOne.question1
-
+triviaGame.hardMode.questionBank.active.activeBank = triviaGame.hardMode.questionBank.bankOne
+triviaGame.hardMode.questionBank.active.activeQuestionCount = triviaGame.hardMode.questionBank.bankOne.length
 // Start of Answer on click
 $("body").on("click",".mycss-AnswerButton",function(){alert("WOW")})
 
